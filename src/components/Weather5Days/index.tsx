8926d2966f4e2f185220e695e4babd73cfb88b2d
mageramarcos <magera.marcos@gmail.com>
2024-09-02 17:04:46 -0300

    chore: Update padding in App.css and style.css
    ```

--- src/components/Weather5Days/index.tsx
+++ src/components/Weather5Days/index.tsx
@@ -2,33 +2,67 @@ import React from "react";
 import './style.css'
 
 interface IWeather5Days {
-    weather5days?:{
+    weather5days?: {
         cnt: number
-        list: string[]
+        list: { dt: number, key: string }[]
     }
 }
 
-const Weather5Days: React.FC<IWeather5Days> = ({weather5days}) => {
-console.log(weather5days);
-console.log(weather5days?.list);
-
-
-// const dailyForecast =  {}
-
-for (const forecast of weather5days?.list) {
-    console.log(forecast);
-    
+interface IDailyForecast {
+    [key: string]: {
+        dt: number,
+        weather?: {
+            icon: string,
+            description: string
+        }[]
+        main?: {
+            temp_min: string
+            temp_max: string
+        }
+    }
 }
 
+interface IConvertDate {
+    dt: number 
+}
 
+const Weather5Days: React.FC<IWeather5Days> = ({ weather5days }) => {
 
-    return(
-    <>
-    <div className="weather5Days-container">
-    <p>card2</p>
-    </div>
-    </>
-)
+    const dailyForecast: IDailyForecast = {}
+
+    for (const forecast of weather5days?.list ?? []) {
+        const date = new Date(forecast.dt * 1000).toLocaleDateString()
+
+        if (!dailyForecast[date]) {
+            dailyForecast[date] = forecast
+        }
+        dailyForecast[date] = forecast
+    }
+
+    const next5daysForecast = Object.values(dailyForecast).slice(1, 6)
+
+    function convertDate(date : IConvertDate) {
+        const newDate = new Date(date.dt * 1000).toLocaleDateString('pt-BR', { weekday: 'long', day: '2-digit' })
+        return newDate
+    }
+
+    return (
+        <>
+            <div className="weather5Days-container">
+                <h3>Previsão Próximos 5 Dias</h3>
+                <div className="wather-list">
+                    {next5daysForecast.map(forecast => (
+                        <div key={forecast.dt} className="weather-item">
+                            <p className="forecast-day">{convertDate(forecast)}</p>
+                            <img className='weather-image'src={`http://openweathermap.org/img/wn/${forecast.weather?.[0]?.icon}.png`} alt="Previsão dia 1º dia" />
+                            <p className="forecast-description">{forecast.weather?.[0]?.description}</p>
+                            <p>{Math.round(Number(forecast.main?.temp_min))}Cº min / {Math.round(Number(forecast.main?.temp_max))}Cº max</p>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default Weather5Days
